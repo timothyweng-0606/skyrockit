d3addcf6dec58ac53cadbaff54ab6210ec23d5fd
Timothy Weng <timothy880606@gmail.com>
2024-07-19 22:51:01 -0500

    complete lab

--- models/user.js
+++ models/user.js
@@ -1,5 +1,26 @@
 const mongoose = require('mongoose');
 
+const applicationSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'],
+  },
+});
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -7,6 +28,11 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
+    required: true,
+  },
+  //use an array and pass the schema 
+  applications: {
+    type: [applicationSchema],
     required: true,
   },
 });
--- server.js
+++ server.js
@@ -6,11 +6,15 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
+const applicationsController = require('./controllers/applications.js');
 
 const authController = require('./controllers/auth.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
-
+const path = require('path');
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
@@ -20,6 +24,9 @@ mongoose.connection.on('connected', () => {
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 // app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -28,21 +35,21 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
-});
+app.use(passUserToView);
 
-app.get('/vip-lounge', (req, res) => {
+app.get('/', (req, res) => {
   if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
+    // Redirect logged-in users to their applications index
+    res.redirect(`/users/${req.session.user._id}/applications`);
   } else {
-    res.send('Sorry, no guests allowed.');
+    // Show the homepage for users who are not logged in
+    res.render('index.ejs');
   }
 });
 
 app.use('/auth', authController);
+app.use(isSignedIn);
+app.use('/users/:userId/applications', applicationsController)
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
